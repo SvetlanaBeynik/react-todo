ba364f76c225d3629216d0a3b3fb86f3f2ce7968
SvetlanaBeynik <beynik.svetlana@gmail.com>
2023-03-14 21:24:24 -0400

    lesson_1_1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React from 'react'
+const todoList = [
+  {
+    id: 1,
+    title: "Complete assignment list"
+  },
+  {
+    id: 2,
+    title: "Complete assignment map"
+  },
+  {
+    id: 3,
+    title: "Complete assignment objects"
+  }
+];
 
 function App() {
+  const listItems = todoList.map((number) =>
+    <li key={number["id"].toString()}>
+      {number["title"]}
+    </li >
+  );
   return (
-    <div style={{ textAlign: 'center' }}>
+    <div style={{ textAlign: 'left' }}>
       <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
+        <div style={{ textAlign: 'center' }}>
+          <h1>Todo List</h1>
+        </div>
+        <ul>{listItems}</ul>
       </header>
     </div>
   );
