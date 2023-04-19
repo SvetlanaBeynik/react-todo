3c8da9ff28b7d1ff8425c5e0e99432d9ad122c85
SvetlanaBeynik <beynik.svetlana@gmail.com>
2023-04-18 21:41:29 -0400

    Lesson-1-5

--- src/App.js
+++ src/App.js
@@ -1,27 +1,53 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 
-function App() {
+function useSemiPersistentState() {
+  const [todoList, setTodoList] = useState(
+    JSON.parse(localStorage.getItem('savedTodoList')) || []
+  );
 
-  const [todoList, setTodoList] = useState([]);
+  useEffect(() => {
+    localStorage.setItem('savedTodoList', JSON.stringify(todoList));
+  }, [todoList]);
+  return [todoList, setTodoList];
+
+
+}
+
+function App() {
+  const [todoList, setTodoList] = useSemiPersistentState();
+
+  // const [todoList, setTodoList] = useState(
+  //   JSON.parse(localStorage.getItem('savedTodoList')));
+
+  // React.useEffect(() => {
+  //   localStorage.setItem('savedTodoList', JSON.stringify(todoList));
+  // }, [todoList]);
 
   function addTodo(newTodo) {
     setTodoList([...todoList, newTodo])
   }
   return (
-    <div style={{ textAlign: 'left' }}>
+    <>
+
       <header>
-        <div style={{ textAlign: 'center' }}>
+
+        <div style={{
+          textAlign: 'left',
+          textAlign: 'center'
+        }}>
           <h1>Todo List</h1>
           <hr />
         </div>
+
 
         <AddTodoForm onAddTodo={addTodo}></AddTodoForm>
         <TodoList todoList={todoList}></TodoList>
 
       </header>
-    </div>
+    </>
+
   );
 }
 
